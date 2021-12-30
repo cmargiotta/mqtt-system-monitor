e4c260eebb65198b3643e0256fd58b185758ac03
Carmine Margiotta <car.margiotta@icloud.com>
2021-12-30 18:09:57 +0100

    Added cpu_temperature default sensor

---
+++ default/sensors/cpu_temperature.lua
@@ -0,0 +1,15 @@
+local open = io.open
+
+local file = open("cat /sys/class/hwmon/hwmon4/temp1_input", "r") 
+local content = file:read() --first line
+
+file:close()
+
+local temp = tonumber(content)/1000
+	
+sensor.value = tostring(temp)
+sensor.debug_message = "WARNING: check if the right hwmon is loaded"
+sensor.name = "CPU Temperature"
+sensor.id = "cpu_temp"
+sensor.unit = "°C"
+sensor.class = "temperature"
