14390027bc9a62a26ac1db3830f951840135afb4
Carmine Margiotta <car.margiotta@icloud.com>
2021-12-30 20:29:52 +0100

    Repaired total memory value

--- default/sensors/total_memory.lua
+++ default/sensors/total_memory.lua
@@ -4,9 +4,9 @@ local file = open("/proc/meminfo", "r")
 local total = string.match(file:read(), "%d+") --first line
 local free = string.match(file:read(), "%d+") --second line
 
-local usage = total/1000000
+local memory = total/1000000
 
-sensor.value = string.format("%.2f", total)
+sensor.value = string.format("%.2f", memory)
 sensor.debug_message = ""
 sensor.name = "Total emory"
 sensor.id = "total_mem"
