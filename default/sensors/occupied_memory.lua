d24fbb764319b87cfe5eae0c097bd02ca07dcd43
MuratovAS <lexamzx@gmail.com>
2022-08-24 14:43:16 +0500

    Update available_memory.lua, memory_usage.lua, and 2 more files...

--- default/sensors/occupied_memory.lua
+++ default/sensors/occupied_memory.lua
@@ -1,12 +1,12 @@
 local open = io.open
-
-local file = open("/proc/meminfo", "r") 
+local file = open("/proc/meminfo", "r")
 local total = string.match(file:read(), "%d+") --first line
 local free = string.match(file:read(), "%d+") --second line
+file:close()
 
 local memory = (total - free)/1000000
 
-sensor.value = string.format("%.2f", memory)
+sensor.value = string.format("%.1f", memory)
 sensor.debug_message = ""
 sensor.name = "Occupied memory"
 sensor.id = "occupied_mem"
--- default/sensors/total_memory.lua
+++ default/sensors/total_memory.lua
@@ -1,12 +1,12 @@
 local open = io.open
-
-local file = open("/proc/meminfo", "r") 
+local file = open("/proc/meminfo", "r")
 local total = string.match(file:read(), "%d+") --first line
 local free = string.match(file:read(), "%d+") --second line
+file:close()
 
 local memory = total/1000000
 
-sensor.value = string.format("%.2f", memory)
+sensor.value = string.format("%.1f", memory)
 sensor.debug_message = ""
 sensor.name = "Total memory"
 sensor.id = "total_mem"
